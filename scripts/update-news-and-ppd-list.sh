7cdc5c80b9cca0453c5380346cf5d1bd64ec0198
Andreas Thalhammer <andreas.thalhammer@linux.com>
2022-11-28 11:59:21 +0100

    script fixes

--- scripts/update-news-and-ppd-list.sh
+++ scripts/update-news-and-ppd-list.sh
@@ -8,12 +8,12 @@ TARGZ_FILE="${TMPDIR}/epson-inkjet-printer-escpr2-${PV}.tar.gz"
 OUTFILE="latest-ppd-list.txt"
 source net-print/epson-inkjet-printer-escpr2/epson-inkjet-printer-escpr2-${PV}.ebuild
 DL_TARGZ=
-test ! -e ${TARGZ_FILE} && DL_TARGZ=1
-test -n ${DL_TARGZ} && wget -O "${TARGZ_FILE}" "${SRC_URI}"
+test ! -e "${TARGZ_FILE}" && DL_TARGZ=1
+test -n "${DL_TARGZ}" && wget -O "${TARGZ_FILE}" "${SRC_URI}"
 tar -xzf "$TARGZ_FILE" --strip-components=1 --wildcards --no-anchored 'NEWS'
 cat << EOF > ${OUTFILE}
 List of PostScript Printer Description files (PPDs) in epson-inkjet-printer-escpr2-${PV}:
 
 EOF
 tar -ztf "$TARGZ_FILE" --wildcards '*.ppd' | sed 's/epson-inkjet-printer-escpr2-.*\/ppd\///' | sed 's/-epson-escpr2-en.ppd//' | tr '-' ' ' | tr '_' ' ' | sort -V - >> ${OUTFILE}
-test -n ${DL_TARGZ} && rm "${TARGZ_FILE}"
+test -n "${DL_TARGZ}" && rm "${TARGZ_FILE}"
